4de0057a0de6c772b41f25c19bdec7ffbe2dea8f
edmondsylar <edmondmusiitwa@gmail.com>
2020-02-08 21:18:12 -0800

    completed project layout

--- about.php
+++ about.php
@@ -9,12 +9,63 @@
     <div class="row d-flex align-items-center justify-content-center">
       <div class="about-content col-lg-12">
         <h1 class="text-white">
-          Contact Us
+          About Us
         </h1>
-        <p class="text-white link-nav"><a href="index.php">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="contact.html"> Contact Us</a></p>
+        <p class="text-white link-nav"><a href="index.php">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="contact.html"> About</a></p>
       </div>
     </div>
   </div>
 </section>
+
+<section class="insurence-one-area section-gap">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-lg-6 insurence-left">
+							<img class="img-fluid img-one" src="assets/img/insurence/about.jpg" alt="">
+						</div>
+						<div class="col-lg-6 insurence-right">
+							<h6 class="text-uppercase">About Us</h6>
+							<h1>Horizon Birding</h1>
+							<p>
+                Dedicated to ensure that your visit is never the last one with us, Horizon birdings is setting the bar for customer service, tour experiences and knowledge based ventures too high for anyone, Our main purpose is to ensure that you have the visit of a life time to our country, we porvide services that varie from tour guidance, vacations, hotel booking and travel guide advisory services. 
+              </p>
+              <h2>Taking a look?</h2>
+							<div class="list-wrap">
+								<ul>
+									<li> <a href="destinations.php">service one</a> </li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>	
+      </section>
+      
+
+      <section class="insurence-two-area pb-120">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-lg-6 insurence-left">
+							<h6 class="text-uppercase">Our Guide</h6>
+							<h1>Christopher Kwesiga</h1>
+							<p>
+              I am specialist in Bird watching. If you really want to see the most Endemic bird species from over 10 national parks and bird viewing site and almost Over 500 bird spicies in uganda?, contact me.
+              I also arrange transport, accomodation and much more.
+							</p>
+							<div class="list-wrap">
+								<ul>
+									<li> <a href="#">facebook</a> </li>
+									<li> <a href="#">twitter</a> </li>
+									<li> <a href="#">instagram</a> </li>
+									<li> <a href="#">linkedIn</a> </li>
+									
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-6 insurence-right">
+							<img class="img-fluid img-two" src="assets/img/insurence/ChrisK1.jpeg" style="height: 500px; width: auto;" alt="">
+						</div>						
+					</div>
+				</div>	
+			</section>
 
 <<?php include_once "includes/footer.php" ?>
