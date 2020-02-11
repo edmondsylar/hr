cf82efe6fb03578b55298a1bf355554366fc7c40
ukwelys <ukwelys@gmail.com>
2020-02-10 22:53:50 -0800

    added db

--- horizon.sql
+++ horizon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2020 at 01:53 PM
+-- Generation Time: Feb 11, 2020 at 07:53 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.26
 
